e5ea2691a333b16f772d96d63d1555625f6911e3
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-04 16:31:11 -0300

    testado relacionamento many to many

---
+++ TarefasHibernate/src/manyToMany/Pessoa.java
@@ -0,0 +1,26 @@
+package manyToMany;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Pessoa {
+	
+	@Id
+	private Long id;
+	
+	private String nome;
+	
+	
+	
+	@ManyToMany
+	@JoinTable( name = "trabalhos_pessoas" )
+	private List<Trabalho> trabalhos; 
+	
+
+}
---
+++ TarefasHibernate/src/manyToMany/Trabalho.java
@@ -0,0 +1,21 @@
+package manyToMany;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Trabalho {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	private String nome;
+	
+	@ManyToMany(mappedBy = "trabalhos")
+	private List<Pessoa> funcionarios;
+}
