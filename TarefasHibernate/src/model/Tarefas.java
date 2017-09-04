dc0dabfa2437e852418bb7dfa7a5efca6d7ad425
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-04 14:23:11 -0300

    testado relação OneToMany

--- TarefasHibernate/src/model/Tarefas.java
+++ TarefasHibernate/src/model/Tarefas.java
@@ -1,11 +1,13 @@
 package model;
 
 import java.util.Calendar;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -13,20 +15,28 @@ import javax.persistence.TemporalType;
 @Entity
 @Table(name = "tarefas")
 public class Tarefas {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
-	
+
 	private String descricao;
 	private boolean finalizado;
-	
+
+	/*
+	 * Desta forma irá criar uma tabela adicional, é preciso modificar.. para o dado
+	 * ser adicionado na tabela Tarefas.
+	 */
+
+	// @OneToMany
+	// private List<fake> inteiros;
+
 	@Temporal(TemporalType.DATE)
-	@Column( name ="data_finalizado", nullable = true)
+	@Column(name = "data_finalizado", nullable = true)
 	private Calendar dataFinalizacao;
-	
+
 	public Tarefas() {
-		
+
 	}
 
 	public Long getId() {
