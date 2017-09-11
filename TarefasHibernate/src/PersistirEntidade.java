f5e6395fe1d26646422a7fd1cb84baae64dcdda1
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-11 15:59:05 -0300

    persistindo tarefa no banco

---
+++ TarefasHibernate/src/PersistirEntidade.java
@@ -0,0 +1,31 @@
+import java.util.Calendar;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Tarefas;
+
+public class PersistirEntidade {
+
+	public static void main(String[] args) {
+		
+		EntityManagerFactory factory =  Persistence.createEntityManagerFactory("tarefas");
+		EntityManager manager = factory.createEntityManager();
+		
+		Tarefas tarefa1 = new Tarefas();
+		tarefa1.setDataFinalizacao("25/09/2017");
+		tarefa1.setDescricao("Testando minha primeira tarefa !!! ");
+		tarefa1.setFinalizado(true);
+	
+		manager.getTransaction().begin();
+		manager.persist(tarefa1);
+		manager.getTransaction().commit();
+		
+		manager.close();
+		factory.close();
+		
+
+	}
+
+}
