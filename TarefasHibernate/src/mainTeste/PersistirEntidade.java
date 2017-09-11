766dc3767d19f20494ca88dba307873fb7155921
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-11 16:45:33 -0300

    adicionado mais registros

--- TarefasHibernate/src/mainTeste/PersistirEntidade.java
+++ TarefasHibernate/src/mainTeste/PersistirEntidade.java
@@ -18,9 +18,21 @@ public class PersistirEntidade {
 		tarefa1.setDataFinalizacao("25/09/2017");
 		tarefa1.setDescricao("Testando minha primeira tarefa !!! ");
 		tarefa1.setFinalizado(true);
-	
+		
+		Tarefas tarefa2 = new Tarefas();
+		tarefa2.setDataFinalizacao("10/02/2016");
+		tarefa2.setDescricao("Tarefa secundária. ");
+		tarefa2.setFinalizado(true);
+		
+		Tarefas tarefa3 = new Tarefas();
+		tarefa3.setDataFinalizacao("02/05/2017");
+		tarefa3.setDescricao("Modificar atualização de registros.");
+		tarefa3.setFinalizado(true);
+		
 		manager.getTransaction().begin();
 		manager.persist(tarefa1);
+		manager.persist(tarefa2);
+		manager.persist(tarefa3);
 		manager.getTransaction().commit();
 		
 		manager.close();
