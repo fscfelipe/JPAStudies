4f5e51bdeb1845017e60da6919268f4babde74aa
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-11 16:36:56 -0300

    modificado classe para testar se foi retornado o registro

--- TarefasHibernate/src/mainTeste/RemovendoEntidade.java
+++ TarefasHibernate/src/mainTeste/RemovendoEntidade.java
@@ -11,11 +11,18 @@ public class RemovendoEntidade {
 		EntityManagerFactory factory =  Persistence.createEntityManagerFactory("tarefas");
 		EntityManager manager = factory.createEntityManager();
 		
-		Tarefas tarefaParaRemover = manager.find(Tarefas.class, 6L);
 		
-		manager.getTransaction().begin();
-		manager.remove(tarefaParaRemover);
-		manager.getTransaction().commit();
+		// É bom sempre antes de remover fazer uma busca para verificar se o dado está registrado
+		// senão, ao tentar excluir um registro que não está no banco, será retornado um exceção.
+		Tarefas tarefaParaRemover = manager.find(Tarefas.class, 7L);
+		
+		if(tarefaParaRemover != null) {
+			manager.getTransaction().begin();
+			manager.remove(tarefaParaRemover);
+			manager.getTransaction().commit();
+		}else {
+			System.out.println("Registro não encontrado !");
+		}
 
 	}
 
