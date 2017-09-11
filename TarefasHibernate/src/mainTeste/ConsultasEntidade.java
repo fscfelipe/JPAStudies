c2cf658bae3616bd35add61c527a6d174c2df19a
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-11 17:45:19 -0300

    adicionado consultas

---
+++ TarefasHibernate/src/mainTeste/ConsultasEntidade.java
@@ -0,0 +1,34 @@
+package mainTeste;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import model.Tarefas;
+
+public class ConsultasEntidade {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory =  Persistence.createEntityManagerFactory("tarefas");
+		EntityManager manager = factory.createEntityManager();
+		
+		// Implementada como um objeto *Query* ou *TypedQuery*
+		// as consultas podem ser determinadas tanto estaticamente (NamedQueries)
+		// como dinamicamente (pode ser emitidade em tempo de execução).
+		
+		// obs.: O nome da tabela na cláusula FROM precisa ser o nome da classe ao qual ela
+		// referência.
+		TypedQuery<Tarefas> consulta = manager.createQuery("SELECT tar FROM Tarefas tar", Tarefas.class);
+		
+		List<Tarefas> lista = consulta.getResultList();
+		
+		for(Tarefas tar: lista) {
+			System.out.println(tar.getDescricao());
+		}
+
+	}
+
+}
