dd9ecad37ed2c7c222bf345405d074aa15cc0e30
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-09-11 16:15:50 -0300

    classe que remove um registro do banco

---
+++ TarefasHibernate/src/RemovendoEntidade.java
@@ -0,0 +1,21 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import model.Tarefas;
+
+public class RemovendoEntidade {
+
+	public static void main(String[] args) {
+		EntityManagerFactory factory =  Persistence.createEntityManagerFactory("tarefas");
+		EntityManager manager = factory.createEntityManager();
+		
+		Tarefas tarefaParaRemover = manager.find(Tarefas.class, 6L);
+		
+		manager.getTransaction().begin();
+		manager.remove(tarefaParaRemover);
+		manager.getTransaction().commit();
+
+	}
+
+}
